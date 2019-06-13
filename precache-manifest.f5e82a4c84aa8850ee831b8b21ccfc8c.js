b382523452882685746785a8ad6631a167def2da
effortsteven <effortsteven@gmail.com>
2019-06-13 09:11:18 +0300

    Updates

---
+++ precache-manifest.f5e82a4c84aa8850ee831b8b21ccfc8c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/ngomecloud/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "e229f31a25d9c76ed6d1",
+    "url": "/ngomecloud/static/css/main.f236a27a.chunk.css"
+  },
+  {
+    "revision": "0b76c963717d708f482a",
+    "url": "/ngomecloud/static/js/runtime~main.0b76c963.js"
+  },
+  {
+    "revision": "94ec444ef278c11105b7",
+    "url": "/ngomecloud/static/js/2.94ec444e.chunk.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/ngomecloud/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/ngomecloud/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/ngomecloud/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/ngomecloud/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/ngomecloud/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/ngomecloud/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/ngomecloud/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/ngomecloud/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/ngomecloud/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/ngomecloud/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "e229f31a25d9c76ed6d1",
+    "url": "/ngomecloud/static/js/main.e229f31a.chunk.js"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/ngomecloud/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/ngomecloud/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/ngomecloud/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/ngomecloud/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/ngomecloud/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/ngomecloud/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/ngomecloud/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/ngomecloud/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/ngomecloud/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/ngomecloud/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/ngomecloud/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/ngomecloud/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "4167149e50f0d3cb1ed761a83f77d7ba",
+    "url": "/ngomecloud/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ngomecloud/precache-manifest.ab114346e9de52f22ef6f2d3e9803aca.js"
+  "/ngomecloud/precache-manifest.f5e82a4c84aa8850ee831b8b21ccfc8c.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ngomecloud/index.html"), {
+workbox.routing.registerNavigationRoute("/ngomecloud/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
